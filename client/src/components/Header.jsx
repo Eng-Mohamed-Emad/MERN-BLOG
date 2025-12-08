fdaf67818dc0790f35f8968b8468b080c6df7b73
Eng-Mohamed-Emad <eng.mohamed.emad111@gmail.com>
2025-12-08 22:04:46 +0200

    create and run the server

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -43,7 +43,9 @@ export default function Header() {
         </Button>
 
         <Link to="/sign-in">
-          <Button gradientDuoTone="purpleToBlue">Sign In</Button>
+          <Button gradientDuoTone="purpleToBlue" outline>
+            Sign In
+          </Button>
         </Link>
 
         <NavbarToggle />

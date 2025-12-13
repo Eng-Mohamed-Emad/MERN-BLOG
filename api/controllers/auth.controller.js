07de2bbcd1861c07033a715e7ef65183e936941a
Eng-Mohamed-Emad <eng.mohamed.emad111@gmail.com>
2025-12-13 22:54:23 +0200

    create signin api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,8 +1,11 @@
 import User from "../models/user.model.js"
 import bcryptjs from "bcryptjs"
 import { errorHandler } from "../utils/error.js"
+import jwt from "jsonwebtoken"
+
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body
+
   if (
     !username ||
     !email ||
@@ -11,20 +14,52 @@ export const signup = async (req, res, next) => {
     email === "" ||
     password === ""
   ) {
-    // return res.status(400).json({ message: "All Fields are Required" })n
-    next(errorHandler(400, "All Fields are Required"))
+    next(errorHandler(400, "All fields are required"))
   }
-  const hashedPassword = await bcryptjs.hash(password, 10)
+
+  const hashedPassword = bcryptjs.hashSync(password, 10)
+
   const newUser = new User({
     username,
     email,
     password: hashedPassword,
   })
+
   try {
     await newUser.save()
-    return res.status(201).json({ message: "User Created Successfully" })
+    res.json("Signup successful")
   } catch (error) {
-    // return res.status(400).json({ message: error.message })
+    next(error)
+  }
+}
+
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body
+
+  if (!email || !password || email === "" || password === "") {
+    next(errorHandler(400, "All fields are required"))
+  }
+
+  try {
+    const validUser = await User.findOne({ email })
+    if (!validUser) {
+      return next(errorHandler(404, "User not found"))
+    }
+    const validPassword = bcryptjs.compareSync(password, validUser.password)
+    if (!validPassword) {
+      return next(errorHandler(400, "Invalid password"))
+    }
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET)
+
+    const { password: pass, ...rest } = validUser._doc
+
+    res
+      .status(200)
+      .cookie("access_token", token, {
+        httpOnly: true,
+      })
+      .json(rest)
+  } catch (error) {
     next(error)
   }
 }

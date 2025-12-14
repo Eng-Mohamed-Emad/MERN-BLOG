2860bf9370e42e40817e9bd90dee984ade280e5d
Eng-Mohamed-Emad <eng.mohamed.emad111@gmail.com>
2025-12-14 04:34:05 +0200

    complete profile page UI

--- client/src/components/DashSidebar.jsx
+++ client/src/components/DashSidebar.jsx
@@ -1,19 +1,7 @@
 import { HiUser, HiArrowSmallRight } from "react-icons/hi2"
-import { useEffect, useState } from "react"
-import { Link, useLocation } from "react-router-dom"
+import { Link } from "react-router-dom"
 
-export default function DashSidebar() {
-  const location = useLocation()
-  const [tab, setTab] = useState("")
-
-  useEffect(() => {
-    const urlParams = new URLSearchParams(location.search)
-    const tabFromUrl = urlParams.get("tab")
-    if (tabFromUrl) {
-      setTab(tabFromUrl)
-    }
-  }, [location.search])
-
+export default function DashSidebar({ tab }) {
   return (
     <div className="w-full md:w-56 bg-gray-50 dark:bg-gray-800 h-full">
       <div className="flex flex-col gap-2 p-4">
